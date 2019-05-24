9e46a32de74e15392e60b7f5dbb334e2c1138f8d
Masanori HIRANO <masa.hirano.1996@gmail.com>
2019-05-24 15:58:04 +0900

    とりあえずFCNエージェントを入れた # ToDo  - FCNの情報遅延，注文可能間隔の低下  - HFTの実装

---
+++ samples/HFThirano2019/HFThirano2019Main.java
@@ -0,0 +1,55 @@
+package samples.HFThirano2019;
+
+import plham.Main;
+import plham.Market;
+import plham.agent.FCNAgent;
+import plham.main.SequentialRunner;
+
+import java.util.List;
+
+public class HFThirano2019Main extends Main {
+    public static void main(String[] args) {
+        final HFThirano2019Main sim = new HFThirano2019Main();
+        FCNAgent.register(sim);
+        Market.register(sim);
+        final SequentialRunner<HFThirano2019Main> runner = new SequentialRunner<HFThirano2019Main>(
+                sim);
+        runner.run(args);
+    }
+
+    public HFThirano2019Main() {
+        // do nothing
+    }
+
+    /*@Override
+    public void print(String sessionName) {
+        super.print(sessionName);
+    }*/
+    @Override
+    public void beginSession(String sessionName) {
+        super.beginSession(sessionName);
+        System.out.println("# SessionName Time MarketId MarketName FundamentalPrice BestBuyPrice BestSellPrice TradeVolume");
+    }
+
+    @Override
+    public void print(String sessionName) {
+        @SuppressWarnings("hiding")
+        List<Market> markets = getMarketsByName("markets");
+        // List<Agent> agents = getAgentsByName("agents");
+
+        assert markets.size() == 2;
+
+        for (Market market : markets) {
+            long t = market.getTime();
+            System.out.println(String.format("%s %s %s %s %s %s %s %s %s ",
+                    sessionName, t, market.id, market.name, market.getFundamentalPrice(t),
+                    market.getPrice(t), market.getBestBuyPrice(), market.getBestSellPrice(),
+                    market.getTradeVolume()));
+        }
+    }
+    @Override
+    public void endprint(String sessionName, long iterationSteps) {
+        super.endprint(sessionName, iterationSteps);
+        System.out.println("# SessionName Time MarketId MarketName FundamentalPrice lastPrice BestBuyPrice BestSellPrice TradeVolume");
+    }
+}
---
+++ samples/HFThirano2019/plot.R
@@ -0,0 +1,27 @@
+args = commandArgs(T)
+if (length(args) < 2) {
+	cat("Usage: Rscript plot.R DATAFILE OUTFILE.png\n");
+	quit()
+}
+
+datafile = args[1]
+pngfile = args[2]
+
+data = read.table(datafile)
+colnames(data) = c('session', 't', 'market.id', 'market.name', 'price.fundam', 'price.last','price.bestbuy', 'price.bestsell', 'volume')
+
+png(pngfile, width=640, height=480)
+
+par(oma = c(0, 0, 0, 2))
+
+plot(0, type='n', xlim=c(1, nrow(data)), ylim=c(395, 405), xlab='t', ylab='price')
+lines(data$price.last, col='green')
+lines(data$price.fundam, col='black')
+lines(data$price.bestbuy, col='red')
+lines(data$price.bestsell, col='blue')
+
+par(new = T)
+barplot(data$volume, axes = FALSE, ylab = "", ylim=c(0, 100), col="blue")
+axis(4)
+
+mtext("volume", side = 4, line = 2)
